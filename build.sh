abf4b0a15701d3b73ac6f1ec30e5eaefba0e9e51
Yasuhiro-gh <Pavlich_eng00@mail.ru>
2024-01-06 16:54:59 +0900

    fix wrong slashes

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 mkdir _build
 cd _build
-g++ -c -Wall -g ..\myfunc.cpp
-g++ -c -Wall -g ..\myprog.cpp
-g++ -g -o myprog.exe .\myprog.o .\myfunc.o
+g++ -c -Wall -g ../myfunc.cpp
+g++ -c -Wall -g ../myprog.cpp
+g++ -g -o myprog.exe myprog.o myfunc.o
 cd ..
